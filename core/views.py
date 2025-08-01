3b7eec4de2b8fdf6e64854b88c09ef73ba713a9c
Marcos22 <marcoscufre04@gmail.com>
2025-07-31 23:03:30 -0300

    mejoras

--- core/views.py
+++ core/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'index.html')
+
+
+def proyecto_cine(request):
+    return render(request, 'proyecto_cine.html')
+
+
+def proyecto_agencia(request):
+    return render(request, 'proyecto_agencia.html')
+
+
+def proyecto_ecommerce(request):
+    return render(request, 'proyecto_ecommerce.html')
